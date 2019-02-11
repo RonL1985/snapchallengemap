c6a26e97f4a15526c48aefe97d3468e02fbab1ea
Ronald Luna <ronaldluna1@gmail.com>
2019-02-11 08:51:47 -0700

    snap challage maps

---
+++ snap.js
@@ -0,0 +1,47 @@
+// Durations are in minutes
+
+var tasks = [
+
+	{
+
+		'name'     : 'Write for Envato Tuts+',
+
+		'duration' : 120
+
+	},
+
+	{
+
+		'name'     : 'Work out',
+
+		'duration' : 60
+
+	},
+
+	{
+
+		'name'     : 'Procrastinate on Duolingo',
+
+		'duration' : 240
+
+	}
+
+
+];
+var map = function (array, callback) {
+
+	var new_array = [];
+
+	array.forEach(function (element, index, array) {
+		new_array.push(callback(element));
+	});
+
+	return new_array;
+
+};
+
+var task_names = map(tasks, function (task) {
+
+	return task.name;
+
+});
