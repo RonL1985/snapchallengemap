23884458a73e40ed3e0aa7c6e99784c10970f305
Ronald Luna <ronaldluna1@gmail.com>
2019-02-12 08:30:14 -0700

    snap challage maps

--- snap.js
+++ snap.js
@@ -44,4 +44,14 @@ var task_names = map(tasks, function (task) {
 
 	return task.name;
 
+	var total = [1, 2, 3, 4, 5].reduce(function (previous, current, index) {
+		var val = previous + current;
+		console.log("The previous value is " + previous +
+			"; the current value is " + current +
+			", and the current iteration is " + (index + 1));
+		return val;
+	}, 0);
+
+	console.log("The loop is done, and the final value is " + total + ".");
+
 });
